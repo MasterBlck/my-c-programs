93a665e8da9aabcd0eed4296f7f97e2abaa56bd0
MasterBlck <198add@gmail.com>
2021-06-13 15:35:06 -0500

    Agregada carpeta de algoritmos

---
+++ algoritmos/sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 500
+void obtenerArregloAleatorio(int *arreglo, int n);
+void imprimirArreglo(int *arreglo, int n);
+void insertSort(int *arreglo, int n);
+
+int main(int argc, char const *argv[]){
+    srand(time(NULL));
+    int arr[N];
+    clock_t t_inicial, t_final;
+    double segs;
+    printf("Arreglo de números:\n");
+    /*Midiendo tiempo de ejecución*/
+    t_inicial = clock();
+    obtenerArregloAleatorio(arr, N);
+    imprimirArreglo(arr, N);
+    t_final = clock();
+
+    segs = (double) t_final - t_inicial / CLOCKS_PER_SEC;
+    printf("%.16g milisegundos\n", segs/1000);
+    return 0;
+}
+
+
+void obtenerArregloAleatorio(int *arreglo, int n){
+    int i = 0;
+    while( i < n ){
+        arreglo[i] = rand()%1024;
+        i++;
+    }
+}
+
+void imprimirArreglo(int *arreglo, int n){
+    int i = 0;
+    while( i < n ){
+        printf("%d\t" , arreglo[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+void insertSort(int *arreglo, int n){
+    int i;
+    for(i = 0; i < n; i++){
+        
+    }
+}
