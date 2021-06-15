93c78dc2ce452f8d2cac3a50222cb4981995e900
masterblck <198add@gmail.com>
2021-06-14 22:48:39 -0500

    agregado algorito de ordenamiento de insertion

--- algoritmos/sort.c
+++ algoritmos/sort.c
@@ -1,7 +1,9 @@
+/*Programa de Ordenamiento de arreglos por insertion sort*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 500
+#define N 10
 void obtenerArregloAleatorio(int *arreglo, int n);
 void imprimirArreglo(int *arreglo, int n);
 void insertSort(int *arreglo, int n);
@@ -11,7 +13,7 @@ int main(int argc, char const *argv[]){
     int arr[N];
     clock_t t_inicial, t_final;
     double segs;
-    printf("Arreglo de números:\n");
+    printf("Arreglo de números desordenados:\n");
     /*Midiendo tiempo de ejecución*/
     t_inicial = clock();
     obtenerArregloAleatorio(arr, N);
@@ -20,6 +22,14 @@ int main(int argc, char const *argv[]){
 
     segs = (double) t_final - t_inicial / CLOCKS_PER_SEC;
     printf("%.16g milisegundos\n", segs/1000);
+
+
+    printf("Ordenando e imprimiendo el arreglo:\n");
+    insertSort(arr, N);
+    printf("Imprimiendo el arreglo ordenado:\n");
+    imprimirArreglo(arr, N);
+
+
     return 0;
 }
 
@@ -42,8 +52,17 @@ void imprimirArreglo(int *arreglo, int n){
 }
 
 void insertSort(int *arreglo, int n){
-    int i;
-    for(i = 0; i < n; i++){
+    int j, i;
+    int key;
+    for(j = 1; j < n; j++){
+        key = arreglo[j];
+        //Inserta A[j] dentro de la secuencia ordenada A[1 .. j-1]
+        i = j - 1;
+        while (i >= 0 && arreglo[i] > key){
+            arreglo[ i + 1 ] = arreglo[i];
+            i = i - 1;
+        }
         
+        arreglo[i + 1] = key;
     }
 }
